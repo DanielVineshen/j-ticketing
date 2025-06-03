5a714291836399bff2c426b2b979d74d5ebfddcf
daniel <danielvineshen@gmail.com>
2025-06-03 20:12:26 +0800

    added create ticket group api v4

--- internal/core/routes/ticket_group_routes.go
+++ internal/core/routes/ticket_group_routes.go
@@ -21,5 +21,4 @@ func SetupTicketGroupRoutes(app *fiber.App, ticketGroupHandler *handlers.TicketG
 	ticketGroup.Get("/attachment/:uniqueExtension", ticketGroupHandler.GetTicketGroupImage)
 
 	ticketGroup.Post("/", middleware.Protected(jwtService), middleware.HasAnyRole("ADMIN", "MEMBER"), ticketGroupHandler.CreateTicketGroup)
-	ticketGroup.Put("/placement", middleware.Protected(jwtService), middleware.HasAnyRole("ADMIN", "MEMBER"), ticketGroupHandler.UpdateTicketGroupPlacement)
 }
